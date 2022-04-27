0320cdcbf9955369e713bd1d5e6e157fad691b97
Rajin <rajink92@gmail.com>
2022-04-27 18:16:18 -0400

    added seeds for quiz and answers

---
+++ db/seeds/04_answers.sql
@@ -0,0 +1,47 @@
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'Victoria', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'Vancouver', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, 'Prince George', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Calgary', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Lethbridge', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, 'Edmonton', TRUE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Saskatoon', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Regina', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, 'Moosejaw', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (4, 'Winnipeg', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (4, 'Brandon', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (4, 'Churchill', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (5, 'Salt Lake City', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (5, 'New Orleans', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (5, 'Phoenix', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (6, 'Toronto', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (6, 'San Francisco', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (6, 'Dallas', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (7, 'Washington DC', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (7, 'New York', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (7, 'Boston', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (8, 'Dallas', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (8, 'Houston', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (8, 'Foxborough', TRUE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (9, 'Ottawa', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (9, 'Vancouver', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (9, 'Toronto', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (10, 'Istanbul', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (10, 'Antalya', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (10, 'Ankara', TRUE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (11, 'Mumbai', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (11, 'New Delhi', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (11, 'Banglore', FALSE);
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (12, 'Washington DC', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (12, 'Philidelhpia', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (12, 'New York', FALSE);
