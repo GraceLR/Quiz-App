1fd6c712041924d5d5db34cfe41c5837f1cce93e
Kanem <kanemdeng@email.com>
2022-04-28 18:21:15 -0700

    some front page, CSS and EJS

--- server.js
+++ server.js
@@ -38,15 +38,19 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
 
+
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
+
 
 app.get("/", (req, res) => {
   res.render("index");
